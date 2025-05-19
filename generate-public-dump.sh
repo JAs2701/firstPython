6c795e9ff88609a75f52d93f5f8d27e3305228aa
Jorge AQUINO SANCHEZ <67315354+JAs2701@users.noreply.github.com>
2025-05-19 11:36:45 +0200

    Update generate-public-dump.sh

--- generate-public-dump.sh
+++ generate-public-dump.sh
@@ -5,7 +5,7 @@ export dir_home=$(dirname $0)
 
 # source 
 export data=$dir_home/dump.ttl
-export sparql=$dir_home/sparql
+export sparql=$dir_home/all.ru
 export output_dir=$dir_home
 
 echo "================ Delete entities & declarations ================"
