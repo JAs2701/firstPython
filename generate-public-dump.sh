e01017690d95226134ea6531cb97dbc985ecb529
Jorge AQUINO SANCHEZ <67315354+JAs2701@users.noreply.github.com>
2025-05-19 11:34:50 +0200

    Add generate public dump shell

---
+++ generate-public-dump.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# path
+export dir_home=$(dirname $0)
+
+# source 
+export data=$dir_home/dump.ttl
+export sparql=$dir_home/sparql
+export output_dir=$dir_home
+
+echo "================ Delete entities & declarations ================"
+count=0
+for query in  $sparql/*.ru
+do
+	echo "Sparql Query: " $query
+	if [ $count -eq 0 ]
+	then
+		# rm -rf $output_dir
+		# mkdir -p $output_dir
+		update --data $data --update $query --dump >> $output_dir/public-dump.ttl
+		count=$((count + 1))		
+	else
+		update --data $output_dir/public-dump.ttl --update $query --dump >> $output_dir/tmp_public-dump.ttl
+		# delete file
+		rm $output_dir/public-dump.ttl
+		#rename file
+		mv $output_dir/tmp_public-dump.ttl $output_dir/public-dump.ttl
+	fi	
+done
