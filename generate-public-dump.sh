41a89fb36bd0073b161423adfd8052aa678b81ef
Jorge AQUINO SANCHEZ <67315354+JAs2701@users.noreply.github.com>
2025-05-19 11:55:58 +0200

    Update generate-public-dump.sh

--- generate-public-dump.sh
+++ generate-public-dump.sh
@@ -7,8 +7,8 @@ export sparql=$dir_home/sparql
 export output_dir=$dir_home
 echo "================ Delete entities & declarations ================"
 count=0
-for query in  $sparql/*.ru
-do^M
+for query in  $sparql/*.ru 
+do
 	echo "Sparql Query: " $query
 	if [ $count -eq 0 ]
 	then
@@ -21,4 +21,4 @@ do^M
 		#rename file
 		mv $output_dir/tmp_public-dump.ttl $output_dir/public-dump.ttl
 	fi	
-done^M
+done
