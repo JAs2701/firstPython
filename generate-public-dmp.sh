843102051d9191180dd66b725926196c77df5111
Jorge AQUINO SANCHEZ <67315354+JAs2701@users.noreply.github.com>
2025-05-20 14:47:47 +0200

    Update generate-public-dmp.sh

--- generate-public-dmp.sh
+++ generate-public-dmp.sh
@@ -1,2 +1,26 @@
 #!/bin/sh
-echo "test"
+echo "================ Delete entities & declarations ================"
+export dir_home=$(dirname $0)
+# source 
+export data=$dir_home/dump.ttl
+# Sparql Queries
+export sparql=$dir_home/sparql
+# Directory Output
+export output_dir=$dir_home
+# Start
+count=0
+for query in  $sparql/*.ru
+do
+	echo "Sparql Query: " $query
+	if [ $count -eq 0 ]
+	then
+		update --data $data --update $query --dump >> $output_dir/public-dump.ttl
+		count=$((count + 1))		
+	else
+		update --data $output_dir/public-dump.ttl --update $query --dump >> $output_dir/tmp_public-dump.ttl
+		# delete file
+		rm $output_dir/public-dump.ttl
+		#rename file
+		mv $output_dir/tmp_public-dump.ttl $output_dir/public-dump.ttl
+	fi	
+done
